4afeb95ac5f165013bc48c504370b83e5fba20c3
befriko <befriko@gmail.com>
2022-11-19 16:39:01 +0700

    Fixed problem with bandpass wavelet
    Fix NameError

--- wedge_utils.py
+++ wedge_utils.py
@@ -1,78 +1,9 @@
-def calc_rc(vp_mod, rho_mod):
-    '''
-    rc_int = calc_rc(vp_mod, rho_mod)
-    '''
-    
-    nlayers = len(vp_mod)
-    nint = nlayers - 1
-    
-    rc_int = []
-    for i in range(0, nint):
-        buf1 = vp_mod[i+1]*rho_mod[i+1]-vp_mod[i]*rho_mod[i]
-        buf2 = vp_mod[i+1]*rho_mod[i+1]+vp_mod[i]*rho_mod[i]
-        buf3 = buf1/buf2
-        rc_int.append(buf3)
-    
-    return rc_int
-    
-def calc_times(z_int, vp_mod):
-    '''
-    t_int = calc_times(z_int, vp_mod)
-    '''
-    
-    nlayers = len(vp_mod)
-    nint = nlayers - 1
-
-    t_int = []
-    for i in range(0, nint):
-        if i == 0:
-            tbuf = z_int[i]/vp_mod[i]
-            t_int.append(tbuf)
-        else:
-            zdiff = z_int[i]-z_int[i-1]
-            tbuf = 2*zdiff/vp_mod[i] + t_int[i-1]
-            t_int.append(tbuf)
-    
-    return t_int
-    
-def digitize_model(rc_int, t_int, t):
-    '''
-    rc = digitize_model(rc, t_int, t)
-    
-    rc = reflection coefficients corresponding to interface times
-    t_int = interface times
-    t = regularly sampled time series defining model sampling
-    '''
-    
-    import numpy as np
-    
-    nlayers = len(rc_int)
-    nint = nlayers - 1
-    nsamp = len(t)
-    
-    rc = list(np.zeros(nsamp,dtype='float'))
-    lyr = 0
-    
-    for i in range(0, nsamp):
-
-        if t[i] >= t_int[lyr]:
-            rc[i] = rc_int[lyr]
-            lyr = lyr + 1    
-
-        if lyr > nint:
-            break
-            
-    return rc
-    
 def plot_vawig(axhdl, data, t, excursion, highlight=None):
 
     import numpy as np
     import matplotlib.pyplot as plt
 
     [ntrc, nsamp] = data.shape
-    
-
-    
     
     t = np.hstack([0, t, t.max()])
     
@@ -96,6 +27,7 @@ def plot_vawig(axhdl, data, t, excursion, highlight=None):
     axhdl.xaxis.tick_top()
     axhdl.xaxis.set_label_position('top')
     axhdl.invert_yaxis()
+
     
 def ricker(cfreq, phase, dt, wvlt_length):
     '''
@@ -120,7 +52,8 @@ def ricker(cfreq, phase, dt, wvlt_length):
     
     t = np.arange(t_min, t_max, dt)
     
-    t = np.linspace(-wvlt_length/2, (wvlt_length-dt)/2, np.rint(wvlt_length/dt).astype(int))
+    #t = np.linspace(-wvlt_length/2, (wvlt_length-dt)/2, np.rint(wvlt_length/dt).astype(int))
+    t = np.linspace(-wvlt_length/2, (wvlt_length-dt)/2, int(wvlt_length/dt))
     wvlt = (1.0 - 2.0*(np.pi**2)*(cfreq**2)*(t**2)) * np.exp(-(np.pi**2)*(cfreq**2)*(t**2))
     
     if phase != 0:
@@ -130,7 +63,8 @@ def ricker(cfreq, phase, dt, wvlt_length):
         wvlt = np.cos(phase)*wvlt - np.sin(phase)*wvlth
     
     return t, wvlt
-    
+
+
 def wvlt_bpass(f1, f2, f3, f4, phase, dt, wvlt_length):
     '''
     Calculate a trapezoidal bandpass wavelet
@@ -148,10 +82,12 @@ def wvlt_bpass(f1, f2, f3, f4, phase, dt, wvlt_length):
     wvlt_length: length of wavelet in seconds
     '''
     
+    import numpy as np
+    import scipy.signal as signal
     from numpy.fft import fft, ifft, fftfreq, fftshift, ifftshift
     
     nsamp = int(wvlt_length/dt + 1)
-    
+    #nsamp = np.rint(wvlt_length/dt + 1).astype(int)
     
     freq = fftfreq(nsamp, dt)
     freq = fftshift(freq)
@@ -194,8 +130,7 @@ def wvlt_bpass(f1, f2, f3, f4, phase, dt, wvlt_length):
     
     # Generate array of wavelet times
     t = np.linspace(-wvlt_length*0.5, wvlt_length*0.5, nsamp)
-    
-    
+        
     # Apply phase rotation if desired
     if phase != 0:
         phase = phase*np.pi/180.0
@@ -204,3 +139,76 @@ def wvlt_bpass(f1, f2, f3, f4, phase, dt, wvlt_length):
         wvlt = np.cos(phase)*wvlt - np.sin(phase)*wvlth
     
     return t, wvlt
+
+
+def calc_rc(vp_mod, rho_mod):
+    '''
+    rc_int = calc_rc(vp_mod, rho_mod)
+    '''
+    
+    import numpy as np
+        
+    nlayers = len(vp_mod)
+    nint = nlayers - 1
+    
+    rc_int = []
+    for i in range(0, nint):
+        buf1 = vp_mod[i+1]*rho_mod[i+1]-vp_mod[i]*rho_mod[i]
+        buf2 = vp_mod[i+1]*rho_mod[i+1]+vp_mod[i]*rho_mod[i]
+        buf3 = buf1/buf2
+        rc_int.append(buf3)
+    
+    return rc_int
+
+
+def calc_times(z_int, vp_mod):
+    '''
+    t_int = calc_times(z_int, vp_mod)
+    '''
+    
+    import numpy as np
+    
+    nlayers = len(vp_mod)
+    nint = nlayers - 1
+
+    t_int = []
+    for i in range(0, nint):
+        if i == 0:
+            tbuf = z_int[i]/vp_mod[i]
+            t_int.append(tbuf)
+        else:
+            zdiff = z_int[i]-z_int[i-1]
+            tbuf = 2*zdiff/vp_mod[i] + t_int[i-1]
+            t_int.append(tbuf)
+    
+    return t_int
+
+
+def digitize_model(rc_int, t_int, t):
+    '''
+    rc = digitize_model(rc, t_int, t)
+    
+    rc = reflection coefficients corresponding to interface times
+    t_int = interface times
+    t = regularly sampled time series defining model sampling
+    '''
+    
+    import numpy as np
+    
+    nlayers = len(rc_int)
+    nint = nlayers - 1
+    nsamp = len(t)
+    
+    rc = list(np.zeros(nsamp,dtype='float'))
+    lyr = 0
+    
+    for i in range(0, nsamp):
+
+        if t[i] >= t_int[lyr]:
+            rc[i] = rc_int[lyr]
+            lyr = lyr + 1    
+
+        if lyr > nint:
+            break
+            
+    return rc
